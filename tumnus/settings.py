53aa6ef4fffb36a41ac7b689913417d9fbed7d40
petraiosklytometis <pedrovmcosta7@gmail.com>
2023-09-08 20:17:47 -0300

    :wrench: chore: static_root, language, time-zone, allowed_hosts

--- tumnus/settings.py
+++ tumnus/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z76^58tc)%71ap3y)j!)xt+pg-9(=*i!#5begvv3)abx2jg#=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
