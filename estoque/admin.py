96553741fb238348ae196ae4f06650868bcbfcac
petraiosklytometis <pedrovmcosta7@gmail.com>
2023-09-10 17:14:31 -0300

    :inbox_tray: feat: Estoque - Categorias, Produtos

--- estoque/admin.py
+++ estoque/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Categoria, Produto
 
+admin.site.register(Categoria)
+admin.site.register(Produto)
 # Register your models here.
--- estoque/models.py
+++ estoque/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.titulo
+    
+class Produto(models.Model):
+    descricao = models.CharField(max_length=40)
+    ean = models.CharField(max_length=13)
+    sku = models.CharField(max_length=20)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    quantidade = models.FloatField()
+    preco_custo = models.FloatField()
+    preco_venda = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.descricao
+    
+    def gerar_desconto(self, desconto):
+        return self.preco_venda - ((self.preco_venda * desconto) / 100)
+    def lucro(self):
+        lucro = self.preco_venda - self.preco_custo
+        return (lucro * 100) / self.preco_custo
 # Create your models here.
